67a57bf8660998d3a5cb72aa5404f140fe6db769
SergM <snmartusenko@gmail.com>
2021-09-02 15:49:18 +0300

    created user CRUD mutation (in schema and resolver)

--- src/resolvers.js
+++ src/resolvers.js
@@ -9,6 +9,33 @@ const resolvers = {
         users: (parent, args, context, info) => {
             return users;
         }
+    },
+
+    Mutation: {
+        createUser(parent, {id, name, email, age}, context, info) {
+            const newUser = {id, name, email, age};
+            users.push(newUser);
+
+            return newUser;
+        },
+        updateUser(parent, {id, name, email, age}, context, info) {
+            const user = users.find(user => user.id === Number.parseInt(id, 10));
+            if (!user) throw new Error("User not found.");
+
+            user.name = name;
+            user.email = email;
+            user.age = age;
+
+            return user;
+        },
+        deleteUser(parent, {id}, context, info) {
+            const userIndex = users.findIndex(user => user.id === id);
+            if (userIndex === -1) throw new Error("User not found.");
+            
+            const deletedUsers = users.splice(userIndex, 1);
+
+            return deletedUsers[0];
+        }
     }
 };
 
